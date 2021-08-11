da778769713a3d53a7fd614f6e4aae97af862f3c
code.wang <code.wang@beibei.com>
2021-08-11 16:56:48 +0800

    init

--- diff.js
+++ diff.js
@@ -1,1 +1,13 @@
-console.log("diff文件")
+const exec = require('child_process').exec;
+const GITDIFF = 'git diff --cached --diff-filter=ACMR --name-only';
+// 执行 git 的命令
+exec(GITDIFF, (error, stdout) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+    }
+    // 对返回结果进行处理，拿到要检查的文件列表
+    const diffFileArray = stdout.split('\n').filter((diffFile) => (
+        /(\.js|\.js)(\n|$)/gi.test(diffFile)
+    ));
+    console.log('待检查的文件：', diffFileArray);
+});
